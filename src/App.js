5472707a5bade09be90d9a9b0c7e307879c907cb
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-12-05 20:37:33 +0200

    graphql tehty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getCountries = gql`
+  {
+    countries {
+      code,  
+      name,
+      continent{name}
+    }
+  }`;
+
+  const { loading, error, data } = useQuery(getCountries);
+
+  if (loading) return <p>Loading...</p>;
+  else if (error) return <p>{error.message}</p>
+  else {
+    return (
+      <div className='App'>
+        <table>
+          <tbody>
+            <tr>
+              <th>Code</th>
+              <th>Name</th>
+              <th>Continent</th>
+            </tr>
+            {
+              data.countries.map((country, index) =>
+                <tr key={index}>
+                  <td>{country.code}</td>
+                  <td>{country.name}</td>
+                  <td>{country.continent.name}</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
